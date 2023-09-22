d1410eb051e875dcef037f336c428154f5e0051d
Nikhil <nikhilchowdary523@icloud.com>
2023-09-22 23:52:25 +0530

    Added Speak,Clear and Stop buttons

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text ,Image, ScrollView} from 'react-native'
+import { View, Text ,Image, ScrollView,TouchableOpacity} from 'react-native'
 import React, { useState } from 'react'
 import AppNavigation from '../navigation'
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
@@ -8,6 +8,14 @@ import Features from '../components/Features';
 import { dummyMessages } from '../constants';
 export default function HomeScreen() {
     const [messages,setMessages]=useState(dummyMessages)
+    const[recording,setRecording]=useState(false );
+    const [speaking,setSpeaking]=useState(true);
+    const clear=()=>{
+        setMessages([]);
+    }
+    const stopSpeaking=()=>{
+        setSpeaking(false);
+    }
   return (
     <View style={tw`flex-1 bg-white`}>
       <SafeAreaView style={tw`flex-1 flex mx-5`}>
@@ -33,11 +41,52 @@ export default function HomeScreen() {
                                     if(message.role=="assistant"){
                                         if(message.content.includes('https')){
                                             //this an ai image
+                                            return(
+                                                <View 
+                                            key={index}
+                                            style={tw`flex-row justify-start`}
+                                            >
+                                                <View style={tw`p-2 flex rounded-2xl bg-emerald-100 rounded-tl-none`}>
+                                                                <Image
+                                                                    source={{ uri: message.content }}
+                                                                    style={[{ width: wp(60), height: wp(60) }, tw`rounded-2xl`]}
+                                                                    resizeMode='contain'
+                                                                    />
+
+
+                                                </View>
+
+                                            </View>
+                                            )
                                         }else{
                                             //text response
+                                            return(
+            
+                                             <View 
+                                             key={index}
+                                             style={{...tw`bg-emerald-100 rounded-xl p-2 rounded-tl-none my-2`, width: wp(70)}}>
+                                                <Text>
+                                                    {message.content}
+
+                                                </Text>
+                                            </View>
+
+                                 
+                                            )
                                         }
                                     }else{
                                         //user input
+                                        return(
+                                        <View key={index} style={tw`flex-row justify-end` } >
+                                             <View style={{...tw`bg-white rounded-xl p-2 rounded-tr-none my-2`, width: wp(70)}}>
+                                                <Text>
+                                                    {message.content}
+
+                                                </Text>
+                                            </View>
+
+                                        </View>
+                                        )
                                     }
                                 })
                             }
@@ -50,6 +99,62 @@ export default function HomeScreen() {
                 <Features />
             )
         }
+
+
+        {
+            /* record, clear and stop button */
+        }
+        <View style={tw`flex justify-center items-center`}>
+            {
+            recording ? (
+                <TouchableOpacity>
+                < Image
+                style={[{ width: hp(10), height: hp(10) }, tw`rounded-full`]}
+                source={require('../../assets/images/voiceLoading.gif')}
+                />
+            </TouchableOpacity>
+            ):(
+                <TouchableOpacity>
+                < Image
+                style={[{ width: hp(10), height: hp(10) }, tw`rounded-full`]}
+                source={require('../../assets/images/recordingIcon.png')}
+                />
+
+                </TouchableOpacity>
+                    
+
+            )
+}
+        {
+            messages.length>0 && 
+            (
+                <TouchableOpacity
+                style={tw`bg-neutral-400 rounded-3xl p-2 absolute right-10`}
+                onPress={clear}
+                >
+                    <Text style={tw`text-white font-semibold `}>
+                    Clear
+                    </Text>
+
+                </TouchableOpacity>
+            )
+        }
+         {
+            speaking && 
+            (
+                <TouchableOpacity
+                style={tw`bg-red-400 rounded-3xl p-2 absolute left-10`}
+                onPress={stopSpeaking}
+                >
+                    <Text style={tw`text-white font-semibold `}>
+                    Stop
+                    </Text>
+
+                </TouchableOpacity>
+            )
+        }
+          
+        </View>
 
       </SafeAreaView>
     </View>
